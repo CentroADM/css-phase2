c0a3538a490efc89ca447a13953fd5d9789655ee
Lunysska <lunysska@gmail.com>
2018-08-20 01:49:31 -0500

    Los últimos cambios para el primer ejemplo de PHP

--- css/comentarios.php
+++ css/comentarios.php
@@ -1,12 +1,12 @@
 <html>
   <body>
     <header>
-      <LINK href="" rel="stylesheet" type="text/css">
+      <LINK href="estilo-comentarios.css" rel="stylesheet" type="text/css">
     </header>
 <!--
     Como te darás cuenta es una estructura simple he usado un fieldset como contenedor, legend para mostrar un titulo y ademas li para todos los elementos del formulario, debes asegurarte que el fieldset tenga como id “form” para poder aplicar el estilo.
   -->
-<form name="frm" method="post" action="envio.php">
+<form name="form" method="get" action="hola.php">
 <fieldset id="form">
 <legend>Mi formulario</legend>
 <ol>
---
+++ css/hola.php
@@ -0,0 +1,13 @@
+<html>
+  <body>
+    <header>
+    	<label>
+        Hola
+        <?php
+          echo $_GET['fname'];
+          
+        ?>
+
+      </label>
+  </body>
+</html>
